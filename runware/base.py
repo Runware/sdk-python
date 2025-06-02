a2341b92ed333b506b195e7c44f35931e4cc9a11
teith <teithant@gmail.com>
2025-06-02 21:06:41 +0300

    fix

--- runware/base.py
+++ runware/base.py
@@ -1075,7 +1075,7 @@ class RunwareBase:
 
         try:
             if self._invalidAPIkey:
-                raise self._invalidAPIkey
+                raise ConnectionError(self._invalidAPIkey)
 
             if not isConnected:
                 await self.connect()
