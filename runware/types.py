dc862f9c0859569ba17175e2902305b12cd409bd
Sirshendu Ganguly <sirshendu.ganguly2015@gmail.com>
2025-12-10 11:30:19 -0500

    Update IInputs.referenceImages to accept Union[str, File, IInputReference]

--- runware/types.py
+++ runware/types.py
@@ -579,7 +579,7 @@ class IInputReference:
 @dataclass
 class IInputs(BaseRequestField):
     references: Optional[List[Union[str, File]]] = field(default_factory=list)
-    referenceImages: Optional[List[IInputReference]] = None
+    referenceImages: Optional[List[Union[str, File, IInputReference]]] = None
     image: Optional[Union[str, File]] = None
     
     @property
@@ -594,7 +594,7 @@ class IInputs(BaseRequestField):
                 stacklevel=3
             )
             if self.referenceImages is None:
-                self.referenceImages = [IInputReference(image=ref) if isinstance(ref, (str, File)) else IInputReference(**ref) for ref in self.references]
+                self.referenceImages = self.references
 
 
 @dataclass
